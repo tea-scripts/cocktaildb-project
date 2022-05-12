1b8a7b2b9d1ceef4fffecb0bf1de6249a4b93097
teamie <timmieprince@gmail.com>
2022-05-13 00:20:49 +0800

    initial commit

---
+++ src/components/CocktailList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Cocktail from './Cocktail';
+import Loading from './Loading';
+import { useGlobalContext } from '../context';
+
+const CocktailList = () => {
+  const { loading, cocktails } = useGlobalContext();
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (cocktails.length < 1) {
+    return (
+      <h2 className="section-title">no cocktails matched your criteria</h2>
+    );
+  }
+  return (
+    <section className="section">
+      <h2 className="section-title">cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((cocktail) => {
+          return <Cocktail key={cocktail.id} {...cocktail} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default CocktailList;
---
+++ src/context.js
@@ -0,0 +1,60 @@
+import React, { useState, useContext, useEffect } from 'react';
+import { useCallback } from 'react';
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('a');
+  const [cocktails, setCocktails] = useState([]);
+
+  const fetchDrinks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const resp = await fetch(`${url}${searchTerm}`);
+      const data = await resp.json();
+      const { drinks } = data;
+      if (drinks) {
+        const newCocktails = drinks.map((drink) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            drink;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            glass: strGlass,
+            info: strAlcoholic,
+          };
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }, [searchTerm]);
+
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm, fetchDrinks]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        loading,
+        cocktails,
+        setSearchTerm,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => useContext(AppContext);
+
+export { AppContext, AppProvider };
